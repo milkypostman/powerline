b613b8f81fb731918987a20d64e365a2f1d4ca10
Donald Curtis <dcurtis@milkbox.net>
2012-07-07 20:48:45 -0500

    Make default mode-line be functions and thus optional.

--- powerline.el
+++ powerline.el
@@ -300,49 +300,104 @@ static char * %s[] = {
 
 
 ;;;###autoload
-(setq-default mode-line-format
-              '("%e"
-                (:eval
-                 (let* ((active (eq (frame-selected-window) (selected-window)))
-                        (face1 (if active 'powerline-active1 'powerline-inactive1))
-                        (face2 (if active 'powerline-active2 'powerline-inactive2))
-                        (lhs (list
-                              (powerline-raw "%*" nil 'l)
-                              (powerline-buffer-size nil 'l)
-                              (powerline-buffer-id nil 'l)
+(defun powerline-default-center ()
+  "Setup a default mode-line with major and minor modes centered."
+  (interactive)
+  (setq-default mode-line-format
+                '("%e"
+                  (:eval
+                   (let* ((active (eq (frame-selected-window) (selected-window)))
+                          (face1 (if active 'powerline-active1 'powerline-inactive1))
+                          (face2 (if active 'powerline-active2 'powerline-inactive2))
+                          (lhs (list
+                                (powerline-raw "%*" nil 'l)
+                                (powerline-buffer-size nil 'l)
+                                (powerline-buffer-id nil 'l)
 
-                              (powerline-raw " ")
-                              (powerline-arrow-right nil face1)
+                                (powerline-raw " ")
+                                (powerline-arrow-right nil face1)
 
-                              (powerline-major-mode face1 'l)
-                              (powerline-minor-modes face1 'l)
-                              (powerline-raw mode-line-process face1 'l)
-                              (powerline-narrow face1 'l)
+                                (powerline-raw mode-line-process face1 'l)
 
-                              (powerline-raw " " face1)
-                              (powerline-arrow-right face1 face2)
+                                (powerline-narrow face1 'l)
 
-                              (powerline-vc face2)
-                              ))
-                        (rhs (list
-                              (powerline-raw global-mode-string face2 'r)
+                                (powerline-vc face1)
+                                ))
+                          (rhs (list
+                                (powerline-raw global-mode-string face2 'r)
 
-                              (powerline-arrow-left face2 face1)
+                                (powerline-raw "%4l" face1 'r)
+                                (powerline-raw ":" face1)
+                                (powerline-raw "%3c" face1 'r)
 
-                              (powerline-raw "%4l" face1 'r)
-                              (powerline-raw ":" face1)
-                              (powerline-raw "%3c" face1 'r)
+                                (powerline-arrow-left face1 nil)
+                                (powerline-raw " ")
+                                (powerline-raw "%6p" nil 'r)
+                                (powerline-hud face2 face1)))
+                          (center (list
+                                   (powerline-raw " " face1)
+                                   (powerline-arrow-right face1 face2)
+                                   (powerline-major-mode face2 'l)
+                                   (powerline-raw " :" face2)
+                                   (powerline-minor-modes face2 'l)
+                                   (powerline-raw " " face2)
+                                   (powerline-arrow-left face2 face1))))
 
-                              (powerline-arrow-left face1 nil)
-                              (powerline-raw " ")
+                     (concat
+                      (powerline-render lhs)
+                      (powerline-fill-center face1 (/ (powerline-width center) 2.0))
+                      (powerline-render center)
+                      (powerline-fill face1 (powerline-width rhs))
+                      (powerline-render rhs)))))))
 
-                              (powerline-raw "%6p" nil 'r)
 
-                              (powerline-hud face2 face1))))
-                   (concat
-                    (powerline-render lhs)
-                    (powerline-fill face2 (powerline-width rhs))
-                    (powerline-render rhs))))))
+;;;###autoload
+(defun powerline-default ()
+  "Setup a default mode-line."
+  (interactive)
+  (setq-default mode-line-format
+                '("%e"
+                  (:eval
+                   (let* ((active (eq (frame-selected-window) (selected-window)))
+                          (face1 (if active 'powerline-active1 'powerline-inactive1))
+                          (face2 (if active 'powerline-active2 'powerline-inactive2))
+                          (lhs (list
+                                (powerline-raw "%*" nil 'l)
+                                (powerline-buffer-size nil 'l)
+                                (powerline-buffer-id nil 'l)
+
+                                (powerline-raw " ")
+                                (powerline-arrow-right nil face1)
+
+                                (powerline-major-mode face1 'l)
+                                (powerline-minor-modes face1 'l)
+                                (powerline-raw mode-line-process face1 'l)
+
+                                (powerline-narrow face1 'l)
+
+                                (powerline-arrow-right face1 face2)
+
+                                (powerline-vc face2)
+                                ))
+                          (rhs (list
+                                (powerline-raw global-mode-string face2 'r)
+
+                                (powerline-arrow-left face2 face1)
+
+                                (powerline-raw "%4l" face1 'r)
+                                (powerline-raw ":" face1)
+                                (powerline-raw "%3c" face1 'r)
+
+                                (powerline-arrow-left face1 nil)
+                                (powerline-raw " ")
+
+                                (powerline-raw "%6p" nil 'r)
+
+                                (powerline-hud face2 face1))))
+                     (concat
+                      (powerline-render lhs)
+                      (powerline-fill face2 (powerline-width rhs))
+                      (powerline-render rhs)))))))
 
 
 (defun pl/render (item)
