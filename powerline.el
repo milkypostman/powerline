0d9c37dad8309b7185c04717f64e463e9751bb58
Donald Curtis <dcurtis@milkbox.net>
2013-04-02 17:52:55 -0500

    reorder functions so default is first

--- powerline.el
+++ powerline.el
@@ -393,69 +393,11 @@ mouse-3: go to end")))
            (eq (pl/minibuffer-selected-window)
                (selected-window)))))
 
-;;;###autoload
-(defun powerline-center-theme ()
-  "Setup a default mode-line with major and minor modes centered."
-  (interactive)
-  (setq-default mode-line-format
-                '("%e"
-                  (:eval
-                   (let* ((active (powerline-selected-window-active))
-                          (mode-line (if active 'mode-line 'mode-line-inactive))
-                          (face1 (if active 'powerline-active1
-                                   'powerline-inactive1))
-                          (face2 (if active 'powerline-active2
-                                   'powerline-inactive2))
-                          (lhs (list
-                                (powerline-raw "%*" nil 'l)
-                                (powerline-buffer-size nil 'l)
-                                (powerline-buffer-id nil 'l)
-
-                                (powerline-raw " ")
-                                (powerline-arrow-right mode-line face1)
-
-                                (powerline-narrow face1 'l)
-
-                                (powerline-vc face1)))
-                          (rhs (list
-                                (powerline-raw global-mode-string face1 'r)
-
-                                (powerline-raw "%4l" face1 'r)
-                                (powerline-raw ":" face1)
-                                (powerline-raw "%3c" face1 'r)
-
-                                (powerline-arrow-left face1 mode-line)
-                                (powerline-raw " ")
-                                (powerline-raw "%6p" nil 'r)
-                                (powerline-hud face2 face1)))
-                          (center (list
-                                   (powerline-raw " " face1)
-                                   (powerline-arrow-right face1 face2)
-                                   (when (boundp 'erc-modified-channels-object)
-                                     (powerline-raw erc-modified-channels-object
-                                                    face2 'l))
-                                   (powerline-major-mode face2 'l)
-                                   (powerline-process face2)
-                                   (powerline-raw " :" face2)
-                                   (powerline-minor-modes face2 'l)
-                                   (powerline-raw " " face2)
-                                   (powerline-arrow-left face2 face1))))
-
-                     (concat
-                      (powerline-render lhs)
-                      (powerline-fill-center face1 (/ (powerline-width center)
-                                                      2.0))
-                      (powerline-render center)
-                      (powerline-fill face1 (powerline-width rhs))
-                      (powerline-render rhs)))))))
-
-;;;###autoload
-(fset 'powerline-default-center 'powerline-center-theme)
-
 (defun powerline-revert ()
   "Revert to the default Emacs mode-line."
   (interactive)
   (setq-default mode-line-format pl/default-mode-line))
+
 
 ;;;###autoload
 (defun powerline-default-theme ()
@@ -522,7 +464,60 @@ mouse-3: go to end")))
 
 
 ;;;###autoload
-(fset 'powerline-default 'powerline-default-theme)
+(defun powerline-center-theme ()
+  "Setup a default mode-line with major and minor modes centered."
+  (interactive)
+  (setq-default mode-line-format
+                '("%e"
+                  (:eval
+                   (let* ((active (powerline-selected-window-active))
+                          (mode-line (if active 'mode-line 'mode-line-inactive))
+                          (face1 (if active 'powerline-active1
+                                   'powerline-inactive1))
+                          (face2 (if active 'powerline-active2
+                                   'powerline-inactive2))
+                          (lhs (list
+                                (powerline-raw "%*" nil 'l)
+                                (powerline-buffer-size nil 'l)
+                                (powerline-buffer-id nil 'l)
+
+                                (powerline-raw " ")
+                                (powerline-arrow-right mode-line face1)
+
+                                (powerline-narrow face1 'l)
+
+                                (powerline-vc face1)))
+                          (rhs (list
+                                (powerline-raw global-mode-string face1 'r)
+
+                                (powerline-raw "%4l" face1 'r)
+                                (powerline-raw ":" face1)
+                                (powerline-raw "%3c" face1 'r)
+
+                                (powerline-arrow-left face1 mode-line)
+                                (powerline-raw " ")
+                                (powerline-raw "%6p" nil 'r)
+                                (powerline-hud face2 face1)))
+                          (center (list
+                                   (powerline-raw " " face1)
+                                   (powerline-arrow-right face1 face2)
+                                   (when (boundp 'erc-modified-channels-object)
+                                     (powerline-raw erc-modified-channels-object
+                                                    face2 'l))
+                                   (powerline-major-mode face2 'l)
+                                   (powerline-process face2)
+                                   (powerline-raw " :" face2)
+                                   (powerline-minor-modes face2 'l)
+                                   (powerline-raw " " face2)
+                                   (powerline-arrow-left face2 face1))))
+
+                     (concat
+                      (powerline-render lhs)
+                      (powerline-fill-center face1 (/ (powerline-width center)
+                                                      2.0))
+                      (powerline-render center)
+                      (powerline-fill face1 (powerline-width rhs))
+                      (powerline-render rhs)))))))
 
 
 ;;;###autoload
