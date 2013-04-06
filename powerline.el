a9e8c9a0bf42c12eb63e68a813c04cfb75d189b5
Donald Curtis <dcurtis@milkbox.net>
2013-04-06 12:10:56 -0500

    add vim theme

--- powerline.el
+++ powerline.el
@@ -522,6 +522,83 @@ mouse-3: go to end")))
                       (powerline-fill face2 (powerline-width rhs))
                       (powerline-render rhs)))))))
 
+;;;###autoload
+(defun powerline-vim-theme ()
+  "Setup a default mode-line."
+  (interactive)
+  (setq-default mode-line-format
+                '("%e"
+                  (:eval
+                   (let* ((active (powerline-selected-window-active))
+                          (mode-line (if active 'mode-line 'mode-line-inactive))
+                          (face1 (if active 'powerline-active1
+                                   'powerline-inactive1))
+                          (face2 (if active 'powerline-active2
+                                   'powerline-inactive2))
+                          (separator-left
+                           (intern (format "powerline-%s-%s"
+                                           powerline-default-separator
+                                           (car powerline-default-separator-dir))))
+                          (separator-right
+                           (intern (format "powerline-%s-%s"
+                                           powerline-default-separator
+                                           (cdr powerline-default-separator-dir))))
+                          (lhs (list
+                                (powerline-buffer-id `(mode-line-buffer-id ,mode-line) 'l)
+
+                                (powerline-raw "[" mode-line 'l)
+                                (powerline-major-mode mode-line)
+                                (powerline-process mode-line)
+                                (powerline-raw "]" mode-line)
+
+                                (when (buffer-modified-p)
+                                  (powerline-raw "[+]" mode-line))
+                                (when buffer-read-only
+                                  (powerline-raw "[RO]" mode-line))
+
+                                (powerline-raw "[%z]" mode-line)
+
+                                ;; (powerline-raw (concat "[" (mode-line-eol-desc) "]") mode-line)
+
+                                (when which-function-mode
+                                  (concat
+                                   " ["
+                                   (powerline-which-func 'which-func nil)
+                                   "]"))
+
+
+                                (when (boundp 'erc-modified-channels-object)
+                                  (powerline-raw erc-modified-channels-object
+                                                 face1 'l))
+
+                                (powerline-raw "[" mode-line 'l)
+                                (powerline-minor-modes mode-line)
+                                (powerline-raw "%n" mode-line)
+                                (powerline-raw "]" mode-line)
+
+
+                                (when (and vc-mode buffer-file-name)
+                                  (let ((backend (vc-backend buffer-file-name)))
+                                    (when backend
+                                      (concat
+                                       (powerline-raw "[" mode-line 'l)
+                                       (powerline-raw (format "%s / %s" backend
+                                                              (vc-working-revision buffer-file-name backend)))
+                                       (powerline-raw "]" mode-line)))))))
+
+                          (rhs (list
+                                (powerline-raw '(10 "%i"))
+                                (powerline-raw global-mode-string mode-line 'r)
+                                (powerline-raw "%l," mode-line 'l)
+                                (powerline-raw (format-mode-line '(10 "%c")))
+
+                                (powerline-raw (replace-regexp-in-string  "%" "%%" (format-mode-line '(-3 "%p"))) mode-line 'r))))
+                     ;;(message "%s %s" separator-left (funcall 'powerline-wave-left mode-line face1))
+                     (concat
+                      (powerline-render lhs)
+                      (powerline-fill mode-line (powerline-width rhs))
+                      (powerline-render rhs)))))))
+
 
 ;;;###autoload
 (defun powerline-center-theme ()
